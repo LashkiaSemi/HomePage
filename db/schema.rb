9e15ed4bee2b260d1a19b75632f3d3236eb868b7
kichinaga <pixo.3303@gmail.com>
2017-11-12 15:13:44 +0900

    fixed conflict

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,13 @@
 
 ActiveRecord::Schema.define(version: 20171112053846) do
 
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.string "contents"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "documents", force: :cascade do |t|
     t.string "name"
     t.string "path"
