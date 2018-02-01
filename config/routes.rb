b70b4d1780152a5ef888443d701b23bfce09f3b2
Takumi Yoshinaga <pixo.3303@gmail.com>
2018-02-01 13:02:41 +0900

    create sessions controller
    Sessionsコントローラの作成
    
    login_formはログイン画面の描画を行い
    loginはログイン処理を行う
    logoutはログアウト処理のみを行う

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+
+
+
+  get '/login', to: 'sessions#login_form'
+  post '/login', to: 'sessions#login'
+  post '/logout', to: 'sessions#logout'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
