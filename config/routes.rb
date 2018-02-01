098c0642ea5b78ba255eb46b0a25f6a12cbb6928
Takumi Yoshinaga <pixo.3303@gmail.com>
2018-02-01 13:26:48 +0900

    create static pages controller
    StaticPagesコントローラの作成
    
    静的なページを管理するコントローラ
    あまり変化しないページをここで管理する

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  root 'static_pages#index'
+
+  
+  get '/activitiy', to: 'static_pages#activitiy'
+  get '/equipment', to: 'static_pages#equipment'
+  get '/publication', to: 'static_pages#publication'
+  get '/job', to: 'static_pages#job'
+  get '/link', to: 'static_pages#link'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
