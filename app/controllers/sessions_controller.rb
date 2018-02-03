4e81bfca47e63e5d8d725f4a0a688d9441af9ce1
Takumi Yoshinaga <pixo.3303@gmail.com>
2018-02-03 13:18:20 +0900

    login logout
    ログイン、ログアウト機能の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,21 @@
 class SessionsController < ApplicationController
   def login_form
   end
-  
+
   def login
+    user = User.find_by(student_id: params[:session][:student_id])
+    if user && user.authenticate(params[:session][:password])
+      flash[:notice] = "ログインしました。"
+      log_in user
+      redirect_to root_url
+    else
+      flash.now[:danger] = "学籍番号かパスワードが間違っています"
+      render action: :login_form
+    end
   end
 
   def logout
+    log_out
+    redirect_to root_url
   end
 end
