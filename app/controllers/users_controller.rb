d5a65b9426dc4a767b375e4f444c64119d4f5cae
Takumi Yoshinaga <pixo.3303@gmail.com>
2018-02-06 21:44:06 +0900

    user action and view
    Userコントローラの各アクション(+α)を実装
    
    UserHelperにはユーザーの権限確認系の関数を主に実装してある。
    自分のパスワードを変更するedit_pass , update_passアクションを実装。
    ユーザーの新規作成はadmin以上の権限に限定、パスワードは初期値をランダムで生成するように。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,79 @@
 class UsersController < ApplicationController
+  before_action :is_user_logged, except: [:index, :show]
+
   def index
+    users = User.joins(:introduction).includes(:introduction)
+    @seconds = users.select { |user| user.introduction.grade == 2 }
+    @thirds = users.select { |user| user.introduction.grade == 3 }
+    @forthes = users.select { |user| user.introduction.grade == 4 }
+    @others = users.select { |user| user.introduction.grade == 0 }
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    if authority_admin?
+      @user = User.new
+      @user.build_introduction
+    else
+      flash[:notice] = "権限がありません。"
+      redirect_to users_url
+    end
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "ユーザーを登録しました。"
+      redirect_to users_url
+    else
+      flash[:danger] = "ユーザーの登録に失敗しました。"
+      render action: :new
+    end
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "ユーザーを編集しました。"
+      redirect_to user_url(@user)
+    else
+      flash[:danger] = "ユーザーの編集に失敗しました。"
+      render action: :edit
+    end
+  end
+
+  def edit_pass
+    @user = User.find(params[:id])
+  end
+
+  def update_pass
+    @user = User.find(params[:id])
+    if @user.authenticate(params[:user][:old_pass]) && @user.update(user_pass_params)
+      flash[:success] = "パスワードを更新しました。"
+      redirect_to user_url(@user)
+    else
+      flash[:danger] = "パスワードの更新に失敗しました。"
+      render action: :edit_pass
+    end
   end
 
   def destroy
+    User.find(params[:id]).destroy
   end
-  
+
+  private
+   def user_params
+     params.require(:user).permit(:name, :role, :student_id, :password, :password_confirmation, introduction_attributes: [:department, :grade, :comments])
+   end
+
+   def user_pass_params
+     params.require(:user).permit(:password, :password_confirmation)
+   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+=begin
+User.create(name: 'test1', role: 'owner', student_id: 'T314001', password: 'pass1', password_confirmation: 'pass1')
+Introduction.create(user_id: 1, department: '工学部', grade: 2, comments: 'test user')
+
+User.create(name: 'test2', role: 'member', student_id: 'T314002', password: 'pass2', password_confirmation: 'pass2')
+Introduction.create(user_id: 2, department: '工学部', grade: 3, comments: 'test user')
+
+User.create(name: 'test3', role: 'member', student_id: 'T314003', password: 'pass3', password_confirmation: 'pass3')
+Introduction.create(user_id: 3, department: '工学部', grade: 4, comments: 'test user')
+
+User.create(name: 'test4', role: 'member', student_id: 'T314004', password: 'pass4', password_confirmation: 'pass4')
+Introduction.create(user_id: 4, department: '工学部', grade: 0, comments: 'test user')
+=end
