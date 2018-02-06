5e4722ba5281dbb3fe3a04b11eac6bd8d6854f08
hato52 <hato.gt@gmail.com>
2018-02-07 01:50:19 +0900

    fix documents column

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180203042403) do
+ActiveRecord::Schema.define(version: 20180206164821) do
 
   create_table "documents", force: :cascade do |t|
     t.string "name"
     t.string "path"
-    t.boolean "boolean"
+    t.boolean "activation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "comments"
   end
 
   create_table "introductions", force: :cascade do |t|
