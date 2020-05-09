6215d79b9b363dbfb5406c6fa6d070933fc3fc94
yawn-yawn-yawn <yawn.yawn.yawn.84@gmail.com>
2020-05-10 00:14:06 +0900

    refresh go

--- go/pkg/usecase/interactor/auth_handler.go
+++
@@ -1,7 +0,0 @@
-package interactor
-
-// AuthHandler パスワードのハッシュとか検証。実装はinfraで行なっています。
-type AuthHandler interface {
-	PasswordHash(pw string) (string, error)
-	PasswordVerify(hash, pw string) error
-}
