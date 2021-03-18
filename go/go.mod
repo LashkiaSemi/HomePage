e99f273ab3753812a461691ee30df23d06e96bb8
akubi0w1 <yawn.yawn.yawn.84@gmail.com>
2021-03-18 15:59:05 +0900

    add dot env

--- go/go.mod
+++ go/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.3.0
 	github.com/pkg/errors v0.9.1
 	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897
 )
--- go/pkg/configs/variable.go
+++ go/pkg/configs/variable.go
@@ -2,40 +2,35 @@ package configs
 
 import (
 	"os"
+	"log"
+	"github.com/joho/godotenv"
 )
 
 // 環境変数から設定する変数たち
 var (
-	DBUser     = DefaultDBUser
-	DBPassword = DefaultDBPassword
-	DBProtocol = DefaultDBProtocol
-	DBTarget   = DefaultDBTarget
-	DBName     = DefaultDBName
+	DBUser string
+	DBPassword string
+	DBProtocol string
+	DBTarget string
+	DBName string
 
-	JWTSecret = DefaultJWTSecret
+	JWTSecret string
 )
 
 // init 環境変数から読み込みが必要なものを初期化
 func init() {
-	// db関連
-	if os.Getenv("MARIA_USER") != "" {
-		DBUser = os.Getenv("MARIA_USER")
-	}
-	if os.Getenv("MARIA_PASSWORD") != "" {
-		DBPassword = os.Getenv("MARIA_PASSWORD")
-	}
-	if os.Getenv("MARIA_PROTOCOL") != "" {
-		DBProtocol = os.Getenv("MARIA_PROTOCOL")
-	}
-	if os.Getenv("MARIA_TARGET") != "" {
-		DBTarget = os.Getenv("MARIA_TARGET")
-	}
-	if os.Getenv("MARIA_DB") != "" {
-		DBName = os.Getenv("MARIA_DB")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("[error] failed to get dotenv file")
 	}
 
+	// db関連
+	DBUser = os.Getenv("MARIA_USER")
+	DBPassword = os.Getenv("MARIA_PASSWORD")
+	DBProtocol = os.Getenv("MARIA_PROTOCOL")
+	DBTarget = os.Getenv("MARIA_TARGET")
+	DBName = os.Getenv("MARIA_DB")
+
 	// jwt
-	if os.Getenv("JWT_SECRET") != "" {
-		JWTSecret = os.Getenv("JWT_SECRET")
-	}
+	JWTSecret = os.Getenv("JWT_SECRET")
 }
